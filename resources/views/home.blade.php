a856c99ce4768baef9886f23e4980b73f9dbd99e
MóniCrack <005.monica.corte@gmail.com>
2025-12-13 17:42:06 +0100

    aplico estilos en el menú y cambio de logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,14 @@
 @section('title', 'Inicio')
 
 @section('content')
-    <h1>Bienvenido a la tienda de libros de segunda mano</h1>
-    <p>Compra y vende libros usados de forma sencilla.</p>
-@endsection
+<div class="text-center mt-5">
+    <h1>Bienvenido a tu tienda de libros de segunda mano</h1>
+    <h3 style="color:#D4AF37;">Compra y vende libros usados de forma sencilla</h3>
 
+    <img src="{{ asset('images/ui/portada.webp') }}"
+        alt="Portada de libros"
+        class="img-fluid mt-4"
+        style="max-width: 500px;">
+</div>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'Tienda de Libros')</title>
+    <title>@yield('title', 'Tienda de Libros de Segunda Mano')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
@@ -15,20 +15,20 @@
         <div class="container-fluid">
             <div>
                 <!-- logo-->
-                <img src="{{ asset('images/ui/logo_blanco.webp') }}" alt="Logo" style="width:90px; height:90px; border-radius:50%;">
+                <img src="{{ asset('images/ui/logo_dorado_moderno.webp') }}" alt="Logo" style="width:100px; height:100px; border-radius:50%;">
             </div>
             <!-- Buscador -->
             <form class="d-flex mx-auto" action="{{ route('books.search') }}" method="GET" style="width: 50%;">
-                <input class="form-control me-2" type="search" name="q" placeholder="Buscar libros..." aria-label="Buscar">
-                <button class="btn btn-outline-light" type="submit">
+                <input class="form-control me-2" type="search" name="q" placeholder="Buscar libros..." aria-label="Buscar" style="color:#D4AF37; border:2px solid #D4AF37;">
+                <button class="btn btn-outline-light" style="color:#D4AF37; border:2px solid #D4AF37;" type="submit">
                     <i class="bi bi-search"></i>
                 </button>
             </form>
             <div>
                  <!-- Botones -->
-                <a href="/login" class="btn btn-outline-light btn-lg"><i class="bi bi-person-fill"></i> Login</a>
-                <a href="#" class="btn btn-outline-light btn-lg"><i class="bi bi-person-circle"></i> Registro</a>
-                <a href="/carrito" class="btn btn-outline-light btn-lg"><i class="bi bi-cart-fill"></i> Mi Carrito</a>
+                <a href="/login" class="btn btn-outline-light btn-lg" style="color:#D4AF37; border:2px solid #D4AF37;"><i class="bi bi-person-fill"></i> Login</a>
+                <a href="#" class="btn btn-outline-light btn-lg" style="color:#D4AF37; border:2px solid #D4AF37;"><i class="bi bi-person-circle"></i> Registro</a>
+                <a href="/carrito" class="btn btn-outline-light btn-lg" style="color:#D4AF37; border:2px solid #D4AF37;"><i class="bi bi-cart-fill"></i> Mi Carrito</a>
 
             </div>
         </div>
